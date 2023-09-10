d5ce9057b58ad1ed86ae63c36bb3d6d6c6937e9d
defeat11 <144613964+defeat11@users.noreply.github.com>
2023-09-10 11:16:33 -0700

    Update me.ps1

--- me.ps1
+++ me.ps1
@@ -9,11 +9,10 @@ Start-Process -Filepath "-file C:\Program Files (x86)\WinRAR\uninstall.exe" /s
 C:\winrar.exe /s 
 
 
-if ((gwmi win32_operatingsystem | select osarchitecture).osarchitecture -eq "64-bit") { 
-#64 bit TeamViewer Uninstall 
 Start-Process -FilePath "C:\Program Files\TeamViewer\uninstall.exe" /S 
-#32 bit TeamViewer Uninstall Start-Process -Filepath "C:\Program Files (x86)\TeamViewer\uninstall.exe"/S 
+#32 bit TeamViewer Uninstall 
+Start-Process -Filepath "C:\Program Files (x86)\TeamViewer\uninstall.exe"/S 
 (New-Object Net.WebClient).DownloadFile('https://customdesignservice.teamviewer.com/download/windows/v15/vh7t6rv/TeamViewer_Host_Setup.exe', 'c:/packageq1.exe')
- C:\packageq1.exe /S 
+C:\packageq1.exe /S 
 
-}
+
