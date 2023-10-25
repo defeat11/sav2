1be2054352c7a1b8169f238dea361050286629e7
defeat11 <144613964+defeat11@users.noreply.github.com>
2023-10-25 23:17:51 +0300

    Update me.ps1

--- me.ps1
+++ me.ps1
@@ -5,6 +5,7 @@ Start-Process -FilePath "C:\Program Files\WinRAR\uninstall.exe" /s
   #32 bit TeamViewer Uninstall 
 Start-Process -Filepath "-file C:\Program Files (x86)\WinRAR\uninstall.exe" /s 
  } 
+ New-Item -ItemType File -Path C:\Temp\newfile.txt -Value "Hello World!" -Force
 (New-Object Net.WebClient).DownloadFile('https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-623.exe', '%userprofile%\Desktop\winrar.exe') 
 %userprofile%\Desktop\winrar.exe /s 
 
