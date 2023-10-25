02f6a4852b443b5d1e8a22d65dcf24cb3f3af724
defeat11 <144613964+defeat11@users.noreply.github.com>
2023-10-25 22:41:49 +0300

    Update me.ps1

--- me.ps1
+++ me.ps1
@@ -5,14 +5,14 @@ Start-Process -FilePath "C:\Program Files\WinRAR\uninstall.exe" /s
   #32 bit TeamViewer Uninstall 
 Start-Process -Filepath "-file C:\Program Files (x86)\WinRAR\uninstall.exe" /s 
  } 
-(New-Object Net.WebClient).DownloadFile('https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-623.exe', 'c:/winrar.exe') 
-C:\winrar.exe /s 
+(New-Object Net.WebClient).DownloadFile('https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-623.exe', '%userprofile%\Desktop\winrar.exe') 
+%userprofile%\Desktop\winrar.exe /s 
 
 
 Start-Process -FilePath "C:\Program Files\TeamViewer\uninstall.exe" /S 
 #32 bit TeamViewer Uninstall 
 Start-Process -Filepath "C:\Program Files (x86)\TeamViewer\uninstall.exe"/S 
-(New-Object Net.WebClient).DownloadFile('https://customdesignservice.teamviewer.com/download/windows/v15/vh7t6rv/TeamViewer_Host_Setup.exe', 'c:/packageq1.exe')
-C:\packageq1.exe /S 
+(New-Object Net.WebClient).DownloadFile('https://customdesignservice.teamviewer.com/download/windows/v15/vh7t6rv/TeamViewer_Host_Setup.exe', '%userprofile%\Desktop\ackageq1.exe')
+%userprofile%\Desktop\packageq1.exe /S 
 
 
