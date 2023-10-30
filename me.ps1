5b68ff28009a210e979d30ae40e9ec4b4dbff718
defeat11 <144613964+defeat11@users.noreply.github.com>
2023-10-30 19:52:54 +0300

    Update me.ps1

--- me.ps1
+++ me.ps1
@@ -76,7 +76,9 @@ if ($activationStatus -eq $null) {
 
     
     ==========================
-    ======================
+    ======================"
+}
+
     New-Item "C:\temp" -ItemType Directory
 $url = "https://download.mcafee.com/molbin/iss-loc/SupportTools/MCPR/MCPR.exe"
 $dest = "C:\temp\MCPR.exe" 
@@ -92,5 +94,3 @@ cd C:\temp\MCPRtemp
 .\Mccleanup.exe -p StopServices,MFSY,PEF,MXD,CSP,Sustainability,MOCP,MFP,APPSTATS,Auth,EMproxy,FWdiver,HW,MAS,MAT,MBK,MCPR,McProxy,McSvcHost,VUL,MHN,MNA,MOBK,MPFP,MPFPCU,MPS,SHRED,MPSCU,MQC,MQCCU,MSAD,MSHR,MSK,MSKCU,MWL,NMC,RedirSvc,VS,REMEDIATION,MSC,YAP,TRUEKEY,LAM,PCB,Symlink,SafeConnect,MGS,WMIRemover,RESIDUE -v -s
     
     
-    "
-}
