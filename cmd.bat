4032cd738b47b6a112d8b5ccf4e034dd3e8dd339
defeat11 <144613964+defeat11@users.noreply.github.com>
2025-01-22 12:34:43 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,1 +1,7 @@
 start cmd.exe
+
+# فتح ستور 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 0
+# تعطييل ستور
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 1
+
