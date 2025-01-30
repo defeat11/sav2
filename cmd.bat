c3e1da5694d422a174228f725adde84a139df64c
defeat11 <144613964+defeat11@users.noreply.github.com>
2025-01-30 11:59:52 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -2,13 +2,15 @@ start cmd.exe
 
 # فتح ستور 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 0
+gpupdate /force
 # تعطييل ستور
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 1
-
+gpupdate /force
 # يوس اس بي
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices" -Name "Deny_all" -Value 0
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR" -Name "Start" -Value 3
-
+gpupdate /force
 # اغلاق 
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices" -Name "Deny_all" -Value 1
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR" -Name "Start" -Value 4
+gpupdate /force
